70fcf04e64d0dd94e7ca718b42d855fe2c90ee30
Mahmoud Atef <mahmoudatif54@yahoo.com>
2022-10-25 17:02:58 +0200

    update eslint file

--- build/src/routes/images/images.js
+++ build/src/routes/images/images.js
@@ -1,37 +1,42 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const sharp_1 = __importDefault(require("sharp"));
-const images = express_1.default.Router();
+'use strict'
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod }
+    }
+Object.defineProperty(exports, '__esModule', { value: true })
+const express_1 = __importDefault(require('express'))
+const sharp_1 = __importDefault(require('sharp'))
+const images = express_1.default.Router()
 images.get('', (req, res) => {
-    const filename = req.query['filename'];
-    const width = req.query['width'];
-    const height = req.query['height'];
-    (0, sharp_1.default)(`assets/images/${filename}.jpg`)
-        .resize(parseInt(width !== null && width !== void 0 ? width : '100'), parseInt(height !== null && height !== void 0 ? height : '100'))
+    const filename = req.query['filename']
+    const width = req.query['width']
+    const height = req.query['height']
+    ;(0, sharp_1.default)(`assets/images/${filename}.jpg`)
+        .resize(
+            parseInt(width !== null && width !== void 0 ? width : '100'),
+            parseInt(height !== null && height !== void 0 ? height : '100')
+        )
         .toFile(`cache/images/${filename}.jpg`, (err, info) => {
-        console.log(`File info ${info}`);
-        if (err) {
-            console.log(`Has Error ${err}`);
-            res.status(404).send(`
+            console.log(`File info ${info}`)
+            if (err) {
+                console.log(`Has Error ${err}`)
+                res.status(404).send(`
         <center>
         <h3>The following error has occurred ${err.message}</h3>
         <h3>please enter the following url form : http://localhost:3000/api/images?filename=imagename&width=200&height=200</h3>
           
         </center>
-        `);
-            return;
-        }
-        res.send(`
+        `)
+                return
+            }
+            res.send(`
     <center>
     <h3> your new image with width ${width} and height ${height}</h3>
         <img src="http://localhost:3000/${filename}.jpg" />
     </center>
-    `);
-    });
+    `)
+        })
     // res.sendFile(`${__dirname}/assets/index.html`)
     // if (filename == undefined) {
     //     fs.access(`cache/images/${filename}.jpg`, fs.constants.F_OK, (err) => {
@@ -41,5 +46,5 @@ images.get('', (req, res) => {
     //         }
     //     })
     // }
-});
-exports.default = images;
+})
+exports.default = images
